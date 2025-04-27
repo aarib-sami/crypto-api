5be926b39849e1af0ebbf1f3d05402f9c0872c87
Aarib Sami <134624373+aarib-sami@users.noreply.github.com>
2025-04-27 18:32:15 -0400

    Initial commit

---
+++ functions/helpers.js
@@ -0,0 +1,23 @@
+function canadaPost(trackingNumber) {
+    
+}
+
+function purolator(trackingNumber) {
+    
+}
+
+function fedex(trackingNumber) {
+    
+}
+
+function ups(trackingNumber) {
+    
+}
+
+function dhl(trackingNumber) {
+
+}
+
+
+
+  module.exports = { formatDateToMatchApiArgument, getLastFridayOrNonHolidayDate, dateToUnixTimestamp, dateToUnixTimestampPlusADay };
