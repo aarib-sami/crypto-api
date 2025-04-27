dad68a637bfce9fad0e0c6800507772589c8d7ea
Aarib Sami <134624373+aarib-sami@users.noreply.github.com>
2025-04-27 18:45:00 -0400

    Fixed testLocal.js

--- functions/testLocal.js
+++ functions/testLocal.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const router = require('./stockinformation'); 
+const router = require('./api'); 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('/.netlify/functions/stockinformation', router);
+app.use('/.netlify/functions/api', router);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
